b7ce0e0a0dbe9134d45197d9b27b3ac981a5e535
n-kawauchi <n.kawauchi@aist.go.jp>
2020-02-21 20:04:34 +0900

    Added settings for ubuntu16.04.

---
+++ ubuntu_1604/u1604-build.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+#
+# Script to build omniORB 4.2.3, omniORBpy 4.2.3 and OpenRTM-aist-Python
+# in python3 environment of ubuntu 16.04.
+#
+# Patch the debian directory of omniORB omniORBpy 4.2.2.
+# Use this to create a deb package for omniORB and omniORBpy 4.2.3
+# in docker environment.
+# After that, create a deb package for Python3 of OpenRTM-aist-Python.
+#
+# Usage:
+#   $ sh u1604-build.sh
+#   $ ls artifacts
+#   libcos4-2_4.2.3-0.1_amd64.deb   libomnithread4-dev_4.2.3-0.1_amd64.deb
+#   omniidl_4.2.3-0.1_amd64.deb     omniorb_4.2.3-0.1_amd64.deb
+#      :
+#   openrtm-aist-python3_1.2.1-1_amd64.deb
+#
+TARGET=omniorb
+OMNI_VER=4.2.3
+DIST_NAME=ubuntu1604
+IMAGE_NAME=rtm-${DIST_NAME}
+
+OMNI_SRC_DIR=${TARGET}-src
+OMNIPY_SRC_DIR=${TARGET}py-src
+CONTAINER_NAME=${IMAGE_NAME}-${DIST_NAME}
+
+printf "sudo password: "
+stty -echo
+read password
+stty echo
+if test -d ${OMNI_SRC_DIR}; then
+  rm -rf ${OMNI_SRC_DIR}
+fi
+mkdir ${OMNI_SRC_DIR}
+if test -d ${OMNIPY_SRC_DIR}; then
+  rm -rf ${OMNIPY_SRC_DIR}
+fi
+mkdir ${OMNIPY_SRC_DIR}
+
+##----- omniORB
+cd ${OMNI_SRC_DIR}
+wget https://sourceforge.net/projects/omniorb/files/omniORB/omniORB-${OMNI_VER}/omniORB-${OMNI_VER}.tar.bz2
+tar xf omniORB-${OMNI_VER}.tar.bz2
+cp ../omniORB-${OMNI_VER}*.patch .
+cd -
+
+##---- omniORBpy
+cd ${OMNIPY_SRC_DIR}
+wget https://sourceforge.net/projects/omniorb/files/omniORBpy/omniORBpy-${OMNI_VER}/omniORBpy-${OMNI_VER}.tar.bz2
+tar xf omniORBpy-${OMNI_VER}.tar.bz2
+cp ../omniORBpy-${OMNI_VER}*.patch .
+cd -
+
+##---- OpenRTM-aist-Python
+git clone https://github.com/n-kawauchi/OpenRTM-aist-Python
+cd OpenRTM-aist-Python
+git checkout python3-deb-pkgs
+cd -
+
+# Source build in docker environment.
+echo "${password}" | sudo -S docker build -t ${IMAGE_NAME} .
+echo "${password}" | sudo -S docker create --name ${CONTAINER_NAME} ${IMAGE_NAME}
+echo "${password}" | sudo -S docker cp ${CONTAINER_NAME}:/root/artifacts .
+echo "${password}" | sudo -S docker rm ${CONTAINER_NAME}
