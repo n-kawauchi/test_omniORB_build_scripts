c5b14401c37be504c1034497111e8f79aebefcb1
n-kawauchi <n.kawauchi@aist.go.jp>
2020-03-16 19:05:21 +0900

    Version number handling has been reviewed.

--- ubuntu_1804/u1804-build.sh
+++ ubuntu_1804/u1804-build.sh
@@ -19,12 +19,12 @@
 TARGET=omniorb
 OMNI_VER=4.2.3
 DIST_NAME=ubuntu1804
-IMAGE_NAME=rtm-${DIST_NAME}
 
 OMNI_SHORT_VER=`echo ${OMNI_VER} | sed 's/\.//g'`
+IMAGE_NAME=${TARGET}${OMNI_SHORT_VER}-${DIST_NAME}
 OMNI_SRC_DIR=${TARGET}-src
 OMNIPY_SRC_DIR=${TARGET}py-src
-CONTAINER_NAME=${IMAGE_NAME}-${DIST_NAME}
+CONTAINER_NAME=rtm-${DIST_NAME}
 
 printf "sudo password: "
 stty -echo
@@ -41,26 +41,24 @@ mkdir ${OMNIPY_SRC_DIR}
 
 ##----- omniORB
 cd ${OMNI_SRC_DIR}
-wget https://sourceforge.net/projects/omniorb/files/omniORB/omniORB-${OMNI_VER}/omniORB-${OMNI_VER}.tar.bz2
-tar xf omniORB-${OMNI_VER}.tar.bz2
+wget -O- https://sourceforge.net/projects/omniorb/files/omniORB/omniORB-${OMNI_VER}/omniORB-${OMNI_VER}.tar.bz2 | tar jxvf -
 cp ../omniORB-${OMNI_VER}*.patch .
 cd -
 
 ##---- omniORBpy
 cd ${OMNIPY_SRC_DIR}
-wget https://sourceforge.net/projects/omniorb/files/omniORBpy/omniORBpy-${OMNI_VER}/omniORBpy-${OMNI_VER}.tar.bz2
-tar xf omniORBpy-${OMNI_VER}.tar.bz2
+wget -O- https://sourceforge.net/projects/omniorb/files/omniORBpy/omniORBpy-${OMNI_VER}/omniORBpy-${OMNI_VER}.tar.bz2 | tar jxvf -
 cp ../omniORBpy-${OMNI_VER}*.patch .
 cd -
 
 ##---- OpenRTM-aist-Python
-git clone https://github.com/n-kawauchi/OpenRTM-aist-Python
+git clone https://github.com/OpenRTM/OpenRTM-aist-Python
 cd OpenRTM-aist-Python
-git checkout python3-deb-pkgs
+git checkout svn/RELENG_1_2
 cd -
 
 # Source build in docker environment.
-echo "${password}" | sudo -S docker build -t ${IMAGE_NAME} .
+echo "${password}" | sudo -S docker build --build-arg OMNI_VER=${OMNI_VER} -t ${IMAGE_NAME} .
 echo "${password}" | sudo -S docker create --name ${CONTAINER_NAME} ${IMAGE_NAME}
 echo "${password}" | sudo -S docker cp ${CONTAINER_NAME}:/root/artifacts .
 echo "${password}" | sudo -S docker rm ${CONTAINER_NAME}
